55d32d01558d2b5b11c23245822fc04d0eb6d106
Danytech Academic <danytechacademic@danytechacademic.com>
2016-10-23 23:55:21 -0500

    Splash screen android

---
+++ app/src/main/java/danytechacademic/com/splashscreen/Main2Activity.java
@@ -0,0 +1,17 @@
+package danytechacademic.com.splashscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
